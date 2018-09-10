f01d4846e107425c257e29378d6738229c5119ca
Fabian P. Schmidt <kerel-fs@gmx.de>
2018-09-11 00:04:40 +0200

    Reformat tools/chocolateyInstall.ps1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,10 @@
-$packageName = 'wings3d'
-$installerType = 'exe'
-$url = 'https://netix.dl.sourceforge.net/project/wings/wings/2.1.7/wings-2.1.7.exe'
-$url64 = 'https://datapacket.dl.sourceforge.net/project/wings/wings/2.1.7/wings-x64-2.1.7.exe'
-$silentArgs = '/S'
-$validExitCodes = @(0)
+$packageArgs = @{
+  PackageName = 'wings3d'
+  FileType = 'exe'
+  SilentArgs = '/S'
+  Url = 'https://netix.dl.sourceforge.net/project/wings/wings/2.1.7/wings-2.1.7.exe'
+  Url64bit = 'https://datapacket.dl.sourceforge.net/project/wings/wings/2.1.7/wings-x64-2.1.7.exe'
+  ValidExidCodes = @(0)
+}
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage @packageArgs
